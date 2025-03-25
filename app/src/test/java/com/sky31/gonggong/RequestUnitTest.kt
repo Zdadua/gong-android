ba0d9d4da3f68d27e83e114177b1e168d2b29f0d
Administrator <2814126219@qq.com>
2025-03-25 17:48:44 +0800

    test(login): 增加login successful的mock测试

--- app/src/test/java/com/sky31/gonggong/RequestUnitTest.kt
+++ app/src/test/java/com/sky31/gonggong/RequestUnitTest.kt
@@ -1,30 +1,61 @@
 package com.sky31.gonggong
 
 import com.sky31.gonggong.entity.ApiLoginResponse
+import com.sky31.gonggong.service.DealRequestService
 import com.sky31.gonggong.service.RequestService
+import com.sky31.gonggong.service.ResultWrapper
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import retrofit2.Call
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class RequestUnitTest {
     private lateinit var mockWebServer: MockWebServer
-    private lateinit var retrofit: Retrofit
+    private lateinit var dealRequestService: DealRequestService
 
     @Before
     fun setup() {
         mockWebServer = MockWebServer()
         mockWebServer.start()
 
-        retrofit = Retrofit.Builder()
+        val authInterceptor = Interceptor { chain ->
+            val originalRequest: Request = chain.request()
+
+            if(originalRequest.method == "GET") {
+                val token = ""
+
+                val newRequest = originalRequest.newBuilder()
+                    .header("Authorization", "Bearer $token")
+                    .build()
+
+                chain.proceed(newRequest)
+            } else {
+                chain.proceed(originalRequest)
+            }
+        }
+
+        val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+            .addInterceptor(authInterceptor)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+
+        val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl(mockWebServer.url("/"))
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+
+        dealRequestService = DealRequestService(retrofit.create(RequestService::class.java))
     }
 
     @After
@@ -33,9 +64,7 @@ class RequestUnitTest {
     }
 
     @Test
-    fun loginTest() {
-        
-        val apiService = retrofit.create(RequestService::class.java)
+    fun loginTest() = runTest {
 
         val jsonResponse = "{\"access_token\": \"114514\", \"expires_in\": \"9999\", \"token_type\": \"Bearer\"}"
         mockWebServer.enqueue(MockResponse()
@@ -43,23 +72,20 @@ class RequestUnitTest {
             .addHeader("Content-Type", "application/json")
             .setBody(jsonResponse))
 
-        val call: Call<ApiLoginResponse> = (apiService as RequestService).login("202105650301", "13586591252Xx")
-        val response: Response<ApiLoginResponse> = call.execute()
+        val result: ResultWrapper<ApiLoginResponse?> = dealRequestService.login("202105650301", "13586591252Xx")
 
-        if(response.isSuccessful) {
-            val apiLoginResponse: ApiLoginResponse? = response.body()
-            if (apiLoginResponse != null) {
-                val accessToken = apiLoginResponse.accessToken
-                val expiresIn = apiLoginResponse.expiresIn
-                val tokenType = apiLoginResponse.tokenType
+        when(result) {
+            is ResultWrapper.Success -> {
+                val data = result.data
 
-                assert(accessToken == "114514")
-                assert(expiresIn == 9999)
-                assert(tokenType == "Bearer")
-
-            } else {
-                assert(false)
+                assert(data?.accessToken == "114514")
+                assert(data?.tokenType == "Bearer")
+                assert(data?.expiresIn == 9999)
             }
+            is ResultWrapper.Error -> TODO()
+            is ResultWrapper.NetworkError -> TODO()
         }
+
+        println(result)
     }
 }
