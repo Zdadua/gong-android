8a3c80e6ea1220bf0f2a9afdee5e7f753ad90b4c
Administrator <2814126219@qq.com>
2025-03-25 17:42:29 +0800

    core: 添加libs.kotlinx.coroutines.test依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
 
     testImplementation(libs.junit)
     testImplementation(libs.squareup.mockwebserver)
+    testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
