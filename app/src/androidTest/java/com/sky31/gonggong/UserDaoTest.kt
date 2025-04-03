91b09d4a8c960403922c7dbab2fc6473e9848c08
Administrator <2814126219@qq.com>
2025-04-03 18:39:01 +0800

    test: 新增publicData的存取测试

--- app/src/androidTest/java/com/sky31/gonggong/UserDaoTest.kt
+++ app/src/androidTest/java/com/sky31/gonggong/UserDaoTest.kt
@@ -5,16 +5,16 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.sky31.gonggong.dao.UserDao
 import com.sky31.gonggong.database.AppDatabase
+import com.sky31.gonggong.entity.CalendarData
+import com.sky31.gonggong.entity.ClassroomData
 import com.sky31.gonggong.entity.InfoData
-import com.sky31.gonggong.entity.UserEntity
+import com.sky31.gonggong.entity.database.UserEntity
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 @RunWith(AndroidJUnit4::class)
 class UserDaoTest {
@@ -31,7 +31,15 @@ class UserDaoTest {
                 .build()
         }
 
-        userDao = db.userDao()
+        userDao = db.getUserDao()
+
+        runBlocking {
+            val testInfo = InfoData("1","1","1","1","1","1","1","1")
+
+            val userEntity = UserEntity("test", "test_token", testInfo)
+
+            userDao.insertUser(userEntity)
+        }
     }
 
     @After
@@ -40,24 +48,39 @@ class UserDaoTest {
     }
 
     @Test
-    fun insertUserAndGetById() = runTest {
-        val userInfo = InfoData(
-            "2021",
-            "zyh",
-            "male",
-            "2000-1-1",
-            "jw",
-            "2",
-            "2019-6-6",
-            "xtu"
+    fun testGetUser() = runTest {
+        val retrieved = userDao.getUserByUid("test")
+
+        assert(retrieved?.uid == "test")
+    }
+
+    @Test
+    fun testUpdateInfo() = runTest {
+        val info = InfoData("2021","1","456","1","1","1","1","1")
+        val rowUpdated = userDao.updateInfo("test", info)
+        println(rowUpdated)
+
+        val retrieved = userDao.getUserByUid("test")
+        assert(retrieved?.info?.studentId == "2021")
+    }
+
+    @Test
+    fun testInsertUser() = runTest {
+        userDao.insertUser(UserEntity("test02", "test_token", null))
+        val retrieved = userDao.getUserByUid("test02")
+
+        assert(retrieved?.uid == "test02")
+    }
+
+    @Test
+    fun testInsertPublicData() = runTest {
+        val today = ClassroomData(
+            "2000-01-01",
+            mapOf("逸夫楼" to listOf(ClassroomData.ClassroomInfo("程序设计", listOf("空","空","空","空","空"))))
         )
-        val user = UserEntity("2021", "testToken", userInfo)
 
-        userDao.insert(user)
-        val retrieved = userDao.getById("2021")
-        val retrievedToken = userDao.getTokenById("2021")
-
-        assertEquals(user, retrieved)
-        assertEquals(retrievedToken, "testToken")
+        userDao.insertPublicData(today, today, CalendarData("2000-02-02", 24, "1"))
+        val retrieved = userDao.getPublicData()
+        assert(retrieved?.calendar?.weeks == 24)
     }
 }
