ec74c1cac0f7f285c397744fac0785ee8404e996
Administrator <2814126219@qq.com>
2025-04-03 18:40:15 +0800

    refactor: 将login和get请求分离

--- app/src/main/java/com/sky31/gonggong/service/DealRequestService.kt
+++ app/src/main/java/com/sky31/gonggong/service/DealRequestService.kt
@@ -1,5 +1,6 @@
 package com.sky31.gonggong.service
 
+import com.sky31.gonggong.dao.UserDao
 import com.sky31.gonggong.entity.ApiLoginResponse
 import com.sky31.gonggong.entity.ApiResponse
 import com.sky31.gonggong.entity.CalendarData
@@ -9,13 +10,30 @@ import com.sky31.gonggong.entity.ExamData
 import com.sky31.gonggong.entity.InfoData
 import com.sky31.gonggong.entity.RankData
 import com.sky31.gonggong.entity.ScoreData
+import retrofit2.Response
 
-class DealRequestService(apiService: RequestService) {
+class DealRequestService(apiService: RequestService, dao: UserDao) {
     private val requestService = apiService
+    private val userDao = dao
 
-    suspend fun login(username: String, password: String): ResultWrapper<ApiLoginResponse?> {
-        return safeApiCall {
-            requestService.login(username, password)
+    private suspend fun <T> getAndStore(apiCall: suspend() -> Response<T>, storage: suspend(data: T?) -> Unit): Boolean {
+        val result = safeApiCall {
+            apiCall()
+        }
+
+        when(result) {
+            is ResultWrapper.Success -> {
+                storage(result.data)
+                return true
+            }
+            is ResultWrapper.Error -> {
+                // TODO: 处理错误
+                return false
+            }
+            is ResultWrapper.NetworkError -> {
+                // TODO: 处理网络错误
+                return false
+            }
         }
     }
 
