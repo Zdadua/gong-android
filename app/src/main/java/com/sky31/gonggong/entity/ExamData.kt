5f0768539965f6eda8e7660025af3cdf624291f5
Administrator <2814126219@qq.com>
2025-04-03 18:35:05 +0800

    fix: 修复数据类中字段为List<ExamElem>

--- app/src/main/java/com/sky31/gonggong/entity/ExamData.kt
+++ app/src/main/java/com/sky31/gonggong/entity/ExamData.kt
@@ -3,11 +3,15 @@ package com.sky31.gonggong.entity
 import com.google.gson.annotations.SerializedName
 
 data class ExamData(
-    val name: String,
-    @SerializedName("start_time")
-    val startTime: String,
-    @SerializedName("end_time")
-    val endTime: String,
-    val location: String,
-    val type: String,
-)
+    val exams: List<ExamElem>
+) {
+    data class ExamElem(
+        val name: String,
+        @SerializedName("start_time")
+        val startTime: String,
+        @SerializedName("end_time")
+        val endTime: String,
+        val location: String,
+        val type: String,
+    )
+}
