afa5fd81da1825989ff3088613e491cc17f0a4da
Administrator <2814126219@qq.com>
2025-05-15 15:11:25 +0800

    fix: 添加MainActivity的RequiresApi注解

--- app/src/main/java/com/sky31/gonggong/MainActivity.kt
+++ app/src/main/java/com/sky31/gonggong/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.sky31.gonggong
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import com.sky31.gonggong.ui.App
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
